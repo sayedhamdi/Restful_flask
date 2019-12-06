97900b4494cd75e4b90ae730c8903ae245a7d0d1
mouheb <benchiekhmouheb@gmail.com>
2019-12-06 18:36:40 +0100

    react sign in form

--- interfacemouheb/src/inscription.js
+++ interfacemouheb/src/inscription.js
@@ -1,44 +1,62 @@
 import React, { useState } from "react"
-import {Form ,Button} from "react-bootstrap"
+import {Form ,Button, Container} from "react-bootstrap"
 
 function Inscription(){
     const [firstname,setfirstname]=useState("")
     const [lastname,setlastname]=useState("")
-    function handlemail(){
+    const [age,setage]=useState("")
+    const [birthday,setbirth]=useState("")
+    const [sex,setsex]=useState("")
+    function handlemail(event){
+        const name=event.target.name
+        switch(name){
+            case "firstname":setfirstname(event.target.value)
+            break
+            case "lastname":setlastname(event.target.value)
+            break
+            case "age":setage(event.target.value)
+            break
+            case "birthday":setbirth(event.target.value)
+            break
+            case "sex":setsex(event.target.value)
+            break
+        }
 
     }
 
 
     return (
         <div>
+            <Container>
             <Form>
                     <Form.Group controlId="name">
                                     <Form.Label>firstname</Form.Label>
-                                    <Form.Control type="text" placeholder="Enter email" value={firstname} onChange={handlemail} />
+                                    <Form.Control name="firstname" type="text" placeholder="Enter email" value={firstname} onChange={handlemail} />
                                    
                     </Form.Group>
                     <Form.Group controlId="name">
                                     <Form.Label>lastname</Form.Label>
-                                    <Form.Control type="text" placeholder="Enter email" value={lastname} onChange={handlemail} />
+                                    <Form.Control name="lastname" type="text" placeholder="Enter email" value={lastname} onChange={handlemail} />
                                    
                     </Form.Group>
                     <Form.Group controlId="name">
                                     <Form.Label>age</Form.Label>
-                                    <Form.Control type="text" placeholder="Enter email" value={age} onChange={handlemail} />
+                                    <Form.Control name="age" type="text" placeholder="Enter email" value={age} onChange={handlemail} />
                                     
                     </Form.Group>
                     <Form.Group controlId="name">
                                     <Form.Label>birthday</Form.Label>
-                                    <Form.Control type="date" placeholder="Enter email" value={birthday} onChange={handlemail} />
+                                    <Form.Control name="birthday" type="date" placeholder="Enter email" value={birthday} onChange={handlemail} />
                                     
                     </Form.Group>
                     <Form.Group controlId="name">
                                     <Form.Label>sex</Form.Label>
-                                    <Form.Check type="radio" label="man" name="sex" value="1" />
-                                    <Form.Check type="radio" label="women" name="sex" value="0"/>
+                                    <Form.Check type="radio" label="man" name="sex" value="1" onChange={handlemail} />
+                                    <Form.Check type="radio" label="women" name="sex" value="0" onChange={handlemail}/>
                                     
                     </Form.Group>
              </Form>
+             </Container>
         </div>
 
     )
--- interfacemouheb/src/login.js
+++ interfacemouheb/src/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react" 
 import {Form , Button} from "react-bootstrap";
+import axios from "axios"
 
 
 
